82f28be7ddd14656d03817c8c186162f2bb135a3
Muralidhar Vatti <muralivatti@gmail.com>
2023-10-04 17:44:52 +0530

    New Tata Cars page added

---
+++ PageObjectModel/src/main/java/pages/TataCarsPage.java
@@ -0,0 +1,14 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+import base.BasePage;
+
+public class TataCarsPage extends BasePage {
+
+	public TataCarsPage(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+}
